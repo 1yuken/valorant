ecafc60cbff09076f99cd457db3576d3411ac9c0
Osman Asanov <anilf.business@gmail.com>
2022-11-16 15:21:55 +0300

    reorganisation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '<h1>Express</h1>' });
+  res.render('index', { title: 'Express' });
 });
+
+/* Страница Карамельки */
+router.get('/chamber', function(req, res, next) {
+  res.send("<h1>Chamber page</h1>")
+});
+
+/* Страница Карамельки */
+router.get('/jett', function(req, res, next) {
+  res.send("<h1>Jett page</h1>")
+});
+
+/* Страница Карамельки */
+router.get('/sage', function(req, res, next) {
+  res.send("<h1>Sage page</h1>")
+});
+
+
 
 module.exports = router;
