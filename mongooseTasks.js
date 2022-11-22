4ea10a116284c8f48b6d87d49b561b27648c9942
Osman Asanov <anilf.business@gmail.com>
2022-11-22 19:17:27 +0300

    mongoose edit

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Val = mongoose.model('Val', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var chamber = new Val({ name: 'Chamber' })
-chamber.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('You want to play? Lets play')
-    }
+schema.methods.meow = function(){
+    console.log(this.get("name") + " shouted 'You want to play? Lets play' ")
+}
+
+var Val = mongoose.model('Val', schema)
+
+var jdm = new Val({ name: 'Chamber' })
+jdm.save(function (err) {
+    jdm.meow()
 })
