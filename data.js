c1fff74ab88819c25ffbc779b8106d33c168985a
Osman Asanov <anilf.business@gmail.com>
2022-11-22 18:57:33 +0300

    6.2

---
+++ data.js
@@ -0,0 +1,23 @@
+var data = [{
+        title: 'Chamber',
+        nick: 'chamber',
+        avatar: '/images/chamber.png',
+        desc: 'Chamber is one of four Agents in VALORANT that serve the role of Sentinel. These Agents focus on defensive play through recon, healing, and similar abilities. Chamber sets himself apart from the other Sentinels by packing an equally powerful punch on offense.'
+        },
+        {
+        title: 'Jett',
+        nick: 'jett',
+        avatar: '/images/jett.jpg',
+        desc: "Representing her home country of South Korea, Jett's agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them."
+        },
+        {
+        title: 'Sage',
+        nick: 'sage',
+        avatar: '/images/Sage.jpg',
+        desc: 'Sage is a VALORANT agent, whose abilities make her a key support in the game. Sage equips various orbs which can slow enemies, heal allies, or erect walls to control the battlefield. Her ultimate, Resurrection, can even bring dead allies back to life, swinging the balance of power in a match in seconds.'
+        }
+];
+    
+    
+module.exports.data = data;
+    
