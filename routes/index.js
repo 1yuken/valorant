93425288f81b277bdd45ceee43e48d036c4ef51d
Osman Asanov <anilf.business@gmail.com>
2022-12-07 19:42:52 +0300

    9.3

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@ var Agent = require("../models/agent").Agent
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Agent.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.cookie('greeting', 'Hi!!!').render('index', {
+        req.session.greeting = "Hi!!!",
+            res.cookie('greeting', 'Hi!!!').render('index', {
             title: 'Express',
             menu: menu
-          });
-        })
-      });
+        });
+    })
+  
+});
+  
 
 module.exports = router;
