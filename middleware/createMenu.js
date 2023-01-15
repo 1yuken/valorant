5390cc98cb848d84e90700126f963046bc27379a
Osman Asanov <anilf.business@gmail.com>
2023-01-15 18:02:26 +0300

    11.4

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,13 +1,14 @@
 var Agent = require("./../models/agent").Agent
 
 
+
 module.exports = function(req,res,next){
-    res.locals.nav = []
+res.locals.nav = []
 
-    Agent.find(null,{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
-        res.locals.nav = result
-        next()
-    })
 
+Agent.find(null,{_id:0,title:1,nick:1},function(err,result){
+if(err) throw err
+res.locals.nav = result
+next()
+})
 }
