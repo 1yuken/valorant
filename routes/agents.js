036267740ae57c0febfa59d6096096e660637306
Osman Asanov <anilf.business@gmail.com>
2023-01-15 17:57:16 +0300

    11.3

--- routes/agents.js
+++ routes/agents.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var checkAuth = require("./../middleware/checkAuth.js");
-var Agent = require("../models/agent").Agent
+var db = require('../mySQLConnect.js');
+// var checkAuth = require("./../middleware/checkAuth.js");
+// var Agent = require("../models/agent").Agent
 // var async = require("async")
 
 /* GET users listing. */
@@ -10,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница агентов */
-router.get('/:nick',checkAuth, function(req, res, next) {
+router.get('/:nick', function(req, res, next) {
     Agent.findOne({nick:req.params.nick}, function(err,agent){
         if(err) return next(err)
         if(!agent) return next(new Error("Нет такого агента в valorant"))
